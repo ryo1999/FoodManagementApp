a5b2e76a88a831cafa6cdb599a3ed8196bf03bf6
ryo1999 <ryo.soccer19990416@gmail.com>
2022-05-14 23:27:06 +0900

    second

---
+++ functions/src/index.js
@@ -0,0 +1,22 @@
+const functions = require("firebase-functions")
+// Expressの読み込み
+const express = require("express")
+const admin = require("firebase-admin")
+admin.initializeApp(functions.config().firebase)
+const db = admin.firestore()
+const app = express()
+
+
+/* usersテーブル作成*/
+//all_userからusersにデータを移行
+app.get("/create_users", async (req, res) => {
+
+    await db.collection("FoodStock").doc("FoodKind").update({5:"sds"})
+    
+    res.end()
+})
+
+// app.use(express.static(path.join(__dirname,"")))
+
+// 出力
+exports.api = functions.https.onRequest(app)
